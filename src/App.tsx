19bb294c8c74fb41ea65cfeb0990b9559a768119
moriroKim <moriroKim@gmail.com>
2024-10-01 17:53:44 +0900

    ✅ feat: Header & Nav UI 추가 & 라이트/다크모드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,52 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Festival from "./pages/Festival";
-import Content from "./pages/Content";
-import Search from "./pages/Search";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Festival from './pages/Festival';
+import Content from './pages/Content';
+import Search from './pages/Search';
+import { useRecoilState } from 'recoil';
+import { useEffect } from 'react';
+import { themeState } from './atoms/atoms';
 
-function App() {
+const App = () => {
+  const [isDarkMode, setIsDarkMode] = useRecoilState(themeState);
+
+  useEffect(() => {
+    const prevModeSetting = localStorage.getItem('THEME-MODE');
+
+    if (!prevModeSetting) {
+      localStorage.setItem('THEME-MODE', 'light');
+      setIsDarkMode(false);
+    } else {
+      setIsDarkMode(prevModeSetting === 'dark');
+    }
+  }, []);
+
+  useEffect(() => {
+    const root = document.documentElement;
+    const body = document.body;
+
+    if (isDarkMode) {
+      root.style.backgroundColor = '#1a202c';
+      body.style.backgroundColor = '#1a202c';
+      body.style.color = '#f7fafc';
+    } else {
+      root.style.backgroundColor = '#f7fafc';
+      body.style.backgroundColor = '#f7fafc';
+      body.style.color = '#1a202c';
+    }
+
+    localStorage.setItem('THEME-MODE', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/festival' element={<Festival />}></Route>
-      <Route path='/content' element={<Content />}></Route>
-      <Route path='/search' element={<Search />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/festival" element={<Festival />} />
+      <Route path="/content" element={<Content />} />
+      <Route path="/search" element={<Search />} />
     </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/components/buttons/ToggleThemeBtn.tsx
@@ -0,0 +1,18 @@
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { themeState } from '../../atoms/atoms';
+import { useRecoilState } from 'recoil';
+
+export default function ToggleThemeBtn() {
+  const [isDarkMode, setIsDarkMode] = useRecoilState(themeState);
+  const handleToggleBtn = () => {
+    setIsDarkMode((prev) => !prev);
+    localStorage.setItem('THEME-MODE', isDarkMode ? 'light' : 'dark');
+  };
+
+  console.log(isDarkMode);
+  return (
+    <button onClick={handleToggleBtn}>
+      {isDarkMode ? <MdLightMode size={26} color="#FAFAFA" /> : <MdDarkMode size={26} color="#212121" />}
+    </button>
+  );
+}
