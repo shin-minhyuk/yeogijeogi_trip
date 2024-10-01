078263f3e50517b33a1f1295665824015a54c0ce
moriroKim <moriroKim@gmail.com>
2024-10-01 17:53:44 +0900

    ✅ feat: Header & Nav UI 추가 & 라이트/다크모드 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <RecoilRoot>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </RecoilRoot>
 );
