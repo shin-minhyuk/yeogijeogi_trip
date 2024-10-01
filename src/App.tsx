b1e19bcfb4e7d31575410dad6fbbe87338a09a2f
shin-minhyuk <afs_style@naver.com>
2024-10-01 12:59:28 +0900

    ✅ feat: browserRouter 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Festival from "./pages/Festival";
+import Content from "./pages/Content";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <Routes>
-      <Route path='/'></Route>
-      <Route path='/festival'></Route>
-      <Route path='/content/:contentId'></Route>
-      <Route path='/search'></Route>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/festival' element={<Festival />}></Route>
+      <Route path='/content/:contentId' element={<Content />}></Route>
+      <Route path='/search' element={<Search />}></Route>
     </Routes>
   );
 }
