42b28db837b5b559be9e57018ac2c180859b3960
shin-minhyuk <afs_style@naver.com>
2024-10-01 23:40:18 +0900

    ✅ feat: 검색 모달창 전역 상태로 변경, 검색 결과 페이지 데이터 패칭 구현, CommonLayout 수정

--- src/atoms/atoms.ts
+++ src/atoms/atoms.ts
@@ -1,6 +1,11 @@
-import { atom } from 'recoil';
+import { atom } from "recoil";
 
 export const themeState = atom({
-  key: 'isDarkMode',
+  key: "isDarkMode",
   default: false,
+});
+
+export const searchModalState = atom({
+  key: "searchModalState", // 이 atom의 고유한 ID
+  default: false, // 기본값
 });
--- src/client/instance.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({
-  baseURL: "http://apis.data.go.kr/B551011/KorService1",
-  timeout: 1000,
-  headers: { "X-Custom-Header": "foobar" },
-});
