06e79f64e7f3d581a6a62e96d7e74beeb08f6334
kiseon77 <kieson.han77@gmail.com>
2024-10-01 13:32:00 +0900

    💄 style: tailwind추가

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      rr: {
+        900: "#212121",
+        800: "#353535",
+        700: "#616161",
+        600: "#757575",
+        500: "#9E9E9E",
+        400: "#BDBDBD",
+        300: "#E0E0E0",
+        200: "#EEEEEE",
+        100: "#F5F5F5",
+        50: "#FAFAFA",
+      },
+
+    },
+  },
+  plugins: [],
+};
+
